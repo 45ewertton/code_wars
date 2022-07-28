bc9071484624eafea5aa5690e15025e9fcffd463
ewertton oliveira <ewertton45mb@gmail.com>
2022-07-28 14:50:40 -0300

    Finishing exercise codewars string mix and alter code exercise counting duplicates

--- counting_duplicates.py
+++ counting_duplicates.py
@@ -6,6 +6,4 @@ def duplicate_count(text):
         if list_char.count(index_char) > 1:
             total_repeat+=1
             
-    return print(total_repeat)
-        
-duplicate_count('9XVEWmVpJVnSaTrTGuYfSMfTzP0yiDH7stzRP44weVcQbPJ4X54ytjGf77yyHQ245hR5i')
+    return total_repeat
--- string_mix.py
+++ string_mix.py
@@ -1,5 +1,23 @@
-# def mix(s1, s2):
-#     s1 = s1.replace(' ', '')
-#     s2 = s2.replace(' ', '')
+def custom_key(elems):
+    return -len(elems), elems.lower()
 
-# mix('aa aaaa bb', 'a ccc ffff cb')
+def mix(s1, s2):
+    s1 = sorted(s1.replace(' ', ''))
+    s2 = sorted(s2.replace(' ', ''))
+    list1 = set(s1)
+    list2 = set(s2)
+    list_sum = set.union(list1, list2)
+    list_result = list()
+
+    for i in list_sum:
+        if i.islower():
+            if s1.count(i) > 1 or s2.count(i) > 1:
+                if s1.count(i) > s2.count(i):
+                    list_result.append('1:'+str(i*s1.count(i)))
+                elif s1.count(i) < s2.count(i):
+                    list_result.append('2:'+str(i*s2.count(i)))
+                elif s1.count(i) == s2.count(i):
+                    list_result.append('=:'+str(i*s1.count(i)))
+
+    list_result = sorted(list_result, key=custom_key)
+    return '/'.join(list_result)
